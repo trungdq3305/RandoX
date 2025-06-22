a47117c94bbeab041a2e82060bbf4196320f9f11
SangDevK18 <sanghn1306@gmail.com>
2025-06-23 02:23:52 +0700

    detail page for product

---
+++ frontend/src/pages/ProductDetail/productDetail.tsx
@@ -0,0 +1,15 @@
+import { Layout } from 'antd';
+import React from 'react';
+import DetailProduct from '../../components/productDetail/detailProduct';
+const { Content } = Layout
+const ProductDetail: React.FC = () => {
+    return (
+        <Layout>
+            <Content>
+                <DetailProduct />
+            </Content>
+        </Layout>
+    )
+};
+
+export default ProductDetail;
