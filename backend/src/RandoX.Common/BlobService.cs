27873747cefb024240d4e376c447d1443902d1f0
trungdq3305 <fccdotm1@gmail.com>
2025-06-30 18:28:30 +0700

    image upload

---
+++ backend/src/RandoX.Common/BlobService.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandoX.Common
+{
+    public class BlobService
+    {
+        private readonly BlobContainerClient _containerClient;
+
+        public BlobService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureBlobStorage:ConnectionString"];
+            var containerName = configuration["AzureBlobStorage:ContainerName"];
+            _containerClient = new BlobContainerClient(connectionString, containerName);
+            _containerClient.CreateIfNotExists();
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+            var blobClient = _containerClient.GetBlobClient(fileName);
+
+            var headers = new BlobHttpHeaders
+            {
+                ContentType = file.ContentType 
+            };
+
+            using (var stream = file.OpenReadStream())
+            {
+                await blobClient.UploadAsync(stream, new BlobUploadOptions
+                {
+                    HttpHeaders = headers
+                });
+            }
+
+            return blobClient.Uri.ToString();
+        }
+
+    }
+}
