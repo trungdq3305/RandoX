80ae6fc89687e803a184121c476eff4d424c15a5
SangDevK18 <sanghn1306@gmail.com>
2025-07-06 23:32:43 +0700

    fix detail page

--- frontend/src/components/productDetail/detailProduct.tsx
+++ frontend/src/components/productDetail/detailProduct.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, InputNumber, Spin, message } from 'antd';
+import { Button, Collapse, Spin, message, type CollapseProps } from 'antd';
 import { useParams, useNavigate } from 'react-router-dom';
 import './productDetail.css';
 import ProductsCardSlider from '../productSlider/productSlider';
@@ -15,9 +15,10 @@ interface Product {
     price: number;
     image?: string[];
     isDeleted: boolean;
-    manufacturerId: string;
-    categoryId: string;
+    manufacturerName: string;
+    categoryName: string;
     quantity: number;
+    discountValue: number;
 }
 
 interface ProductListResponse {
@@ -40,7 +41,7 @@ interface ProductResponse {
 const DetailProduct: React.FC = () => {
     const { id } = useParams<{ id: string }>();
     const navigate = useNavigate();
-    const [stock, setStock] = useState(1);
+    const [stock] = useState(1);
     const [addToCart] = useAddProductToCartMutation();
 
 
@@ -96,6 +97,82 @@ const DetailProduct: React.FC = () => {
             navigate('/checkout');
         }
     };
+    const children = (
+        <div >
+            <p style={{ fontWeight: "lighter", fontStyle: "oblique" }}>
+                Bộ sưu tập: {product.categoryName}
+            </p>
+            <p style={{ fontWeight: "lighter", fontStyle: "oblique" }}>
+                Mô tả: {product.description}
+            </p>
+            <p style={{ fontWeight: "lighter", fontStyle: "oblique" }}>
+                Nhà phát hành: {product.manufacturerName}
+            </p>
+            <p style={{ color: '#F57F17' }}>
+                Giảm giá: {product.discountValue * 100}%
+            </p>
+        </div>
+    )
+    const shipping = (
+        <>
+            <p style={{ color: '#333', fontSize: '16px' }}>1. Shipping:</p>
+            <ul style={{ listStyleType: 'none', paddingLeft: '0', fontSize: '14px' }}>
+                <li style={{ marginBottom: '10px' }}><strong>Standard Shipping</strong> (15-30 working days)</li>
+                <li style={{ marginBottom: '10px' }}><strong>Expedited Shipping</strong> (3-7 working days)</li>
+            </ul>
+
+            <p style={{ color: '#333', fontSize: '16px' }}>2. Defects:</p>
+            <ul style={{ listStyleType: 'none', paddingLeft: '0', fontSize: '14px' }}>
+                <li style={{ marginBottom: '10px' }}>
+                    (1) Limited items don't have an exchange service. If you find a serious defect in the item, contact{' '}
+                    <a href="mailto:support@popmart.com" style={{ color: '#0066cc', textDecoration: 'none' }}>
+                        support@popmart.com
+                    </a>, with the related order number and unpacking video of the figurine within seven(7) days of receiving the product.
+                </li>
+                <li style={{ marginBottom: '10px' }}>
+                    (2) For information security reasons, please contact us with the email address you used to place the order. You will be contacted by a Customer Support Representative who will assist you in resolving the issue.
+                </li>
+                <li style={{ marginBottom: '10px' }}>
+                    (3) In order to avoid any disputes regarding refunds or exchanges, customers are encouraged to video record themselves opening the package. A video recording must be completed no later than two days after delivery. A video should clearly show the shipping sheet, the package condition, and product defects.
+                </li>
+            </ul>
+
+            <p style={{ color: '#333', fontSize: '16px' }}>3. Tax Fees:</p>
+            <ul style={{ listStyleType: 'none', paddingLeft: '0', fontSize: '14px' }}>
+                <li style={{ marginBottom: '10px' }}>For international express shipments, the buyer shall bear any Customs Duty costs.</li>
+            </ul>
+
+            <p style={{ color: '#333', fontSize: '16px' }}>4. Stock Availability:</p>
+            <ul style={{ listStyleType: 'none', paddingLeft: '0', fontSize: '14px' }}>
+                <li style={{ marginBottom: '10px' }}>Due to special circumstances with the products, there might be occasions where items are out of stock.</li>
+            </ul>
+
+            <p style={{ color: '#333', fontSize: '16px' }}>5. Stock Shortages:</p>
+            <ul style={{ listStyleType: 'none', paddingLeft: '0', fontSize: '14px' }}>
+                <li style={{ marginBottom: '10px' }}>In the event of stock shortages, the unavailable items will be automatically refunded, and the rest of your order will be dispatched as usual.</li>
+            </ul>
+
+            <p style={{ color: '#333', fontSize: '16px' }}>6. Notice:</p>
+            <ul style={{ listStyleType: 'none', paddingLeft: '0', fontSize: '14px' }}>
+                <li style={{ marginBottom: '10px' }}>To ensure fast and safe delivery of this item, we will match the best logistics channel for you. Shipping fees will be automatically calculated at checkout page based on the weight, dimensions, and delivery distance of the product.</li>
+            </ul>
+        </>
+    );
+
+    const items: CollapseProps['items'] = [
+        {
+            key: '1',
+            label: 'Chi tiêt',
+            children: <>
+                {children}
+            </>,
+        },
+        {
+            key: '2',
+            label: 'Vận chuyển & Dịch vụ chăm sóc khách hàng',
+            children: <>{shipping}</>,
+        },
+    ];
 
     return (
         <div
@@ -136,19 +213,19 @@ const DetailProduct: React.FC = () => {
                     />
                 </div>
                 <div className="product-info">
-                    <div className="new-tag">NEW</div>
+                    <div className="new-tag">{product.discountValue ? `-${product.discountValue * 100}%` : "NEW"}</div>
                     <h1>{product.productName}</h1>
-                    <p className="price">{product.price.toLocaleString('vi-VN')} đ</p>
-                    <div className="quantity-selector">
-                        <span>Quantity</span>
-                        <InputNumber
-                            min={1}
-                            max={product.quantity}
-                            value={stock}
-                            onChange={(value) => setStock(value || 1)}
-                            disabled={product.isDeleted}
-                        />
-                    </div>
+                    <p className="price">
+                        {product.discountValue ? (
+                            <div style={{ display: "flex", gap: "10px" }}>
+                                <p style={{ textDecoration: "line-through", color: "gray" }}>
+                                    {product.price.toLocaleString('vi-VN')}đ
+                                </p>
+                                <p>
+                                    {(product.price - product.price * product.discountValue).toLocaleString('vi-VN')}đ
+                                </p>
+                            </div>
+                        ) : product.price.toLocaleString('vi-VN')} </p>
                     <div className="actions">
                         <Button
                             type="primary"
@@ -170,22 +247,8 @@ const DetailProduct: React.FC = () => {
                         </Button>
                     </div>
                     <div className="details-section">
-                        <h3>Details</h3>
-                        <p><strong>Brand:</strong> {product.categoryId}</p>
-                        <a href="#product-details">View Product Details</a> |{' '}
-                        <a href="#blind-box">What's a blind box</a>
+                        <Collapse defaultActiveKey={['1']} items={items} expandIconPosition='end' bordered={true} />
                     </div>
-                    <div className="shipping-section">
-                        <h3>SHIPPING & AFTER - SALES SERVICE</h3>
-                        {/* Add shipping details here */}
-                    </div>
-                    <Button
-                        type="default"
-                        onClick={() => navigate('/cart')}
-                        style={{ marginTop: '16px' }}
-                    >
-                        View Cart
-                    </Button>
                 </div>
             </div>
             <Content title="CÁC SẢN PHẨM" btnContent="Xem thêm" linkURL="/sessions" />
