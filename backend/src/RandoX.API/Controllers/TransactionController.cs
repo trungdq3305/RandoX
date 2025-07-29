860dd273728179284f9872653c72bab49ac25510
trungdq3305 <fccdotm1@gmail.com>
2025-07-30 01:11:56 +0700

    .

--- backend/src/RandoX.API/Controllers/TransactionController.cs
+++ backend/src/RandoX.API/Controllers/TransactionController.cs
@@ -63,8 +63,8 @@ namespace RandoX.API.Controllers
                     OrderId = order.Id.ToString(),
                     Amount = (decimal)order.TotalAmount,
                     Description = $"Thanh toan don hang {order.Id}",
-                    ReturnUrl = "http://localhost:5713/payment-success",
-                    CancelUrl = "http://localhost:5713/payment-cancel"
+                    ReturnUrl = "https://randoxfe.vercel.app/payment-success",
+                    CancelUrl = "https://randoxfe.vercel.app/payment-cancel"
                 };
 
                 // Gọi service tạo link thanh toán
